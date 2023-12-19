faffb91ae5f5152724cb1d060eccf3851ff62497
Octavio Corzo <ocorzo-2021084@kinal.edu.gt>
2023-12-19 16:00:08 -0600

    Service Worker: Fetch

--- 03-service-worker/js/app.js
+++ 03-service-worker/js/app.js
@@ -1,6 +1,8 @@
-
-
 // Detectar si podemos usar Service Workers
-if ( navigator.serviceWorker ) {
-    navigator.serviceWorker.register('/sw.js');
+if (navigator.serviceWorker) {
+  navigator.serviceWorker.register("/sw.js");
 }
+
+fetch("https://reqres.in/api/users")
+  .then((resp) => resp.text())
+  .then(console.log);
--- 03-service-worker/sw.js
+++ 03-service-worker/sw.js
@@ -1,3 +1,36 @@
-
 // Ciclo de vida del SW
 
+self.addEventListener("install", (event) => {
+  console.log("Instalando SW!");
+
+  const instalacion = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log("SW: Instalaciones terminadas");
+      //Actualizacion del service worker sin cerrar las tabs
+      self.skipWaiting();
+      resolve();
+    }, 0);
+  });
+
+  //Espera hasta que termine de realizar un proceso
+  event.waitUntil(instalacion);
+});
+
+// Cuando el SW toma control de la app
+self.addEventListener("activate", (event) => {
+  //Borrar cache viejo
+  console.log("SW2: Activo y listo para controlar la app!!!");
+});
+
+//Fetch: Manejo de peticiones HTTP
+
+self.addEventListener("fetch", (event) => {
+  //Aplicar estrategias del cache
+  console.log("SW", event.request.url);
+
+  if (event.request.url.includes("https://reqres.in/")) {
+    const resp = new Response(`{ok: false, mensaje: 'jajaja'}`);
+
+    event.respondWith(resp);
+  }
+});
