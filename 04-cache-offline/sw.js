12d1744d98ea9f81cf4f86e3b7b3247d020d7200
Octavio Corzo <ocorzo-2021084@kinal.edu.gt>
2024-01-03 11:36:23 -0600

    Cache Storage basico

--- 04-cache-offline/sw.js
+++ 04-cache-offline/sw.js
@@ -1,13 +1,37 @@
 //Detectar cuando no hay conexion o cuando la red falla
 
 self.addEventListener("fetch", (event) => {
-  const offlineResp = new Response(`
-    
-        Bienvenido a mi pagina web
+  // const offlineResp = new Response(`
 
-        Disculpa, pero para usarla, necesitas tener conexion a internet
-    
-    `);
+  //       Bienvenido a mi pagina web
+
+  //       Disculpa, pero para usarla, necesitas tener conexion a internet
+
+  //   `);
+
+  // const offlineResp = new Response(
+  //   `
+  // <!DOCTYPE html>
+  // <html lang="en">
+  // <head>
+  //     <meta charset="UTF-8">
+  //     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  //     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  //     <title>Mi PWA</title>
+  // </head>
+  // <body class="container p-3">
+  //   <h1>Offline Mode</h1>
+  // </body>
+  // </html>
+  // `,
+  //   {
+  //     headers: {
+  //       "Content-Type": "text/html",
+  //     },
+  //   }
+  // );
+
+  const offlineResp = fetch("pages/offline.html");
 
   const resp = fetch(event.request).catch(() => {
     return offlineResp;
