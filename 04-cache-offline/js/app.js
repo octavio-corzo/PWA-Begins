8c30502f70b8a7ca6c483b46e8f6d2310b42046b
Octavio Corzo <ocorzo-2021084@kinal.edu.gt>
2024-01-09 17:00:39 -0600

    Some Cache details

--- 04-cache-offline/js/app.js
+++ 04-cache-offline/js/app.js
@@ -2,38 +2,38 @@ if (navigator.serviceWorker) {
   navigator.serviceWorker.register("/sw.js");
 }
 
-if (window.caches) {
-  //Abre un cache y si no exite lo crea
-  caches.open("prueba-1");
-  caches.open("prueba-2");
+// if (window.caches) {
+//   //Abre un cache y si no exite lo crea
+//   caches.open("prueba-1");
+//   caches.open("prueba-2");
 
-  //Verifica si exite el cache
-  caches.has("prueba-2").then(console.log);
+//   //Verifica si exite el cache
+//   caches.has("prueba-2").then(console.log);
 
-  //   //Borra un cache
-  //   caches.delete("prueba-1").then(console.log);
+//   //   //Borra un cache
+//   //   caches.delete("prueba-1").then(console.log);
 
-  caches.open("cache-v1.1").then((cache) => {
-    //Agrega lo que queramos al cache
-    cache.add("/index.html");
+//   caches.open("cache-v1.1").then((cache) => {
+//     //Agrega lo que queramos al cache
+//     cache.add("/index.html");
 
-    //AddAll agrega nuevos caches para trabajar con el cache lo hacemos con una promesa
-    cache.addAll(["/index.html", "/img/main.jpg"]).then(() => {
-      //Eliminar un cache
-      //   cache.delete("/index.html");
+//     //AddAll agrega nuevos caches para trabajar con el cache lo hacemos con una promesa
+//     cache.addAll(["/index.html", "/img/main.jpg"]).then(() => {
+//       //Eliminar un cache
+//       //   cache.delete("/index.html");
 
-      //Remplazar cualquier cosa que se encuentre en el cache
-      cache.put("index.html", new Response("Hola mundo"));
-    });
+//       //Remplazar cualquier cosa que se encuentre en el cache
+//       cache.put("index.html", new Response("Hola mundo"));
+//     });
 
-    // Leer un archivo que se encuentra en el cache
-    cache.match("/index.html").then((res) => {
-      res.text().then(console.log);
-    });
-  });
+//     // Leer un archivo que se encuentra en el cache
+//     cache.match("/index.html").then((res) => {
+//       res.text().then(console.log);
+//     });
+//   });
 
-  //Obtener todos los caches que existen
-  caches.keys().then((keys) => {
-    console.log(keys);
-  });
-}
+//   //Obtener todos los caches que existen
+//   caches.keys().then((keys) => {
+//     console.log(keys);
+//   });
+// }
--- 04-cache-offline/sw.js
+++ 04-cache-offline/sw.js
@@ -1,41 +1,19 @@
-//Detectar cuando no hay conexion o cuando la red falla
-
-self.addEventListener("fetch", (event) => {
-  // const offlineResp = new Response(`
-
-  //       Bienvenido a mi pagina web
-
-  //       Disculpa, pero para usarla, necesitas tener conexion a internet
-
-  //   `);
-
-  // const offlineResp = new Response(
-  //   `
-  // <!DOCTYPE html>
-  // <html lang="en">
-  // <head>
-  //     <meta charset="UTF-8">
-  //     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  //     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  //     <title>Mi PWA</title>
-  // </head>
-  // <body class="container p-3">
-  //   <h1>Offline Mode</h1>
-  // </body>
-  // </html>
-  // `,
-  //   {
-  //     headers: {
-  //       "Content-Type": "text/html",
-  //     },
-  //   }
-  // );
-
-  const offlineResp = fetch("pages/offline.html");
-
-  const resp = fetch(event.request).catch(() => {
-    return offlineResp;
+self.addEventListener("install", (e) => {
+  const cacheProm = caches.open("cache-1").then((cache) => {
+    return cache.addAll([
+      "/",
+      "/index.html",
+      "/css/style.css",
+      "/img/main.jpg",
+      "https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css",
+      "/js/app.js",
+    ]);
   });
 
-  event.respondWith(resp);
+  e.waitUntil(cacheProm);
+});
+
+self.addEventListener("fetch", (e) => {
+  //1 - Cache Only es usada cuando queremos que toda la app sea accedida desde el cache.
+  e.respondWith(caches.match(e.request));
 });
